89a15333ceff1554aca487a47b7afbc1ef6409c2
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-07-29 12:19:26 -0600

    Initial commit

---
+++ scripts/Phase01.js
@@ -0,0 +1,27 @@
+`* High level object oriented, multi-paradigm programming languaje.
+ * Can add dynamic effects to web pages and create web applications.`
+
+//Variables
+let year = 2024;
+console.log(`The current year is ${year}.`);
+console.log(typeof year);
+
+//Constants
+const pi = 3.1416;
+console.log(`The value of pi is ${pi}.`);
+console.log(typeof pi);
+
+//Data types
+let name = "John Doe";
+console.log(`The name is ${name}.`);
+console.log(typeof name);
+
+//Conditional statements
+const myAge = 52;
+const adultAge = 18;
+if (myAge >= adultAge) {
+  console.log(`You are an adult. Since you are ${myAge} years old.`);
+}
+else {
+  console.log(`You are not an adult. Since you are ${myAge} years old.`);
+}
