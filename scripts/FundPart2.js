a3d4f26329a2312763cfc0fba892525f8cc97c11
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-08-01 17:34:06 -0600

    Added functions introduction

--- scripts/FundPart2.js
+++ scripts/FundPart2.js
@@ -1,3 +1,16 @@
 // Strict mode
 'use strict';
 
+// Functions
+// Function declaration
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Function expression
+const add = function(a, b) {
+    return a + b;
+};
+
+// Arrow function
+const multiply = (a, b) => a * b;
