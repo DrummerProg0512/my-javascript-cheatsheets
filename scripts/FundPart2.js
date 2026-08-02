2667b7068e73dda8c9028975f4f38e23ef726a6a
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-08-01 21:24:02 -0600

    Functions Calling other Functions

--- scripts/FundPart2.js
+++ scripts/FundPart2.js
@@ -25,3 +25,16 @@ const multi2 = (a, b) => {
     return result;
 };
 console.log(multi2(4, 6));
+
+// Functions calling other functions ////////////////////////////////////////////
+function square(num) {
+    return num * num;
+}
+
+console.log(square(5));
+
+function callingOtherFunction(num) {
+    const squared = square(num);
+    return `The square of ${num} is ${squared}.`;
+}
+console.log(callingOtherFunction(5));
